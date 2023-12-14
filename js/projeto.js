888b9acb5fe41e04936b340461dc0b099a661c82
Reverton Silva <revertong@gmail.com>
2023-12-13 18:46:53 -0700

    edicoes pendentes

--- js/projeto.js
+++ js/projeto.js
@@ -1,27 +1,6 @@
-const check_btn = document.getElementById('check_btn')
 const btnSalvar = document.querySelector('.btn_salvar')
 const editor_background = document.getElementById('editor_texto')
 let codigos_salvos = localStorage.getItem('codigos_salvos') ? JSON.parse(localStorage.getItem('codigos_salvos')) : [];
-
-if(!check_btn == null){
-check_btn.addEventListener('click', () => {
-    if(check_btn.classList.contains('btn_like'))
-        ativa_like(false)
-    else
-        ativa_like(true)
-})
-
-function ativa_like (estado){
-    if(estado == false){
-        check_btn.classList.remove("btn_like")
-        check_btn.classList.add("btn_like_red")
-    }
-    else{
-        check_btn.classList.remove("btn_like_red")
-        check_btn.classList.add("btn_like")
-    }
-}
-}
 
 btnSalvar.addEventListener('click',() => {
     const nome_projeto = document.getElementById("input_nome").value
